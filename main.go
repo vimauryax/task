6bebfce32c6c46eda32fa4f7a2ff97e321f4e409
vimauryax <vikashmaurya@pacefin.in>
2025-09-04 14:04:07 +0530

    env

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
-	_"fmt"
+	"fmt"
 	_"net/http"
 	"mv/mvto-do/routes"
 	"github.com/gin-gonic/gin"
 	"mv/mvto-do/config"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 
 func main(){
+	err := godotenv.Load(".env")
+
+	if err!=nil{
+		fmt.Println("could not read the environment from .env")
+	}
+
+	port := os.Getenv("port")
+	environment := os.Getenv("GO_ENV")
+
+	if environment == "local"{
+		fmt.Println("Running in the local environment")
+	}
+
 	config.InitPostgresDBGorm()
 
 	r := gin.Default()
 
 	routes.Routes(r)
-	r.Run("localhost:8081")
+	r.Run("localhost:"+port)
 }
